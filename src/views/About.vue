0df58d7df2826573ea59d29a4dcaa3759b0118bf
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-12-30 01:24:42 -0800

    Basic game functionality working

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <p>This game is based off of the dual n-back test. This type of game has <a href="https://www.npr.org/sections/health-shots/2017/10/23/558767704/in-memory-training-smackdown-one-method-dominates">
+      been shown to help attention and memory
+    </a>. It's called "dual" because you have to remember both position and number. It's called n-back because you have to
+      remember these things n steps back.</p>
+    <p>The goal is to remember the position and number of boxes that are NBack steps back in history. For example if the
+      NBack level is 2 and the number being shown is 5, the previous number was 0, and the one before that was 5, you should
+      hit the right arrow.</p>
+    <p>You will get a point for each position or color entered correctly. You will lose a point for each position or number
+      you miss.</p>
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,13 @@
 <template>
 <div class="page">
+  <div class="score">NBack:
+    <select v-model.number="nBackLevel">
+      <option>1</option>
+      <option>2</option>
+      <option>3</option>
+      <option>4</option>
+      <option>5</option>
+    </select> - Points: {{ points }}</div>
   <table class="grid">
     <tr class="grid__row" v-for="row in rows" v-bind:key="row">
       <td
@@ -13,8 +21,8 @@
     </tr>
   </table>
   <div class="controls">
-    <div class="controls__left">Left</div>
-    <div class="controls__right">Right</div>
+    <div @click="userSelectedPosition" class="controls__left">← Position</div>
+    <div @click="userSelectedNumber" class="controls__right">Number →</div>
   </div>
 </div>
 </template>
@@ -28,24 +36,108 @@ export default {
       rows: 3,
       selectedRow: 2,
       selectedColumn: 2,
-      selectedNumber: 5
+      selectedNumber: 5,
+      nBackLevel: 1,
+      points: 0
     };
   },
   created() {
-    console.log("created");
-    this.engine = setInterval(this.createSelection, 100);
+    window.addEventListener("keydown", this.onKey);
+
+    this.interval = 2000;
+    this.history = [];
+    this.tries = {
+      number: false,
+      position: false
+    };
+    this.pushToHistory();
+    this.engine = setInterval(this.createSelection, this.interval);
   },
   methods: {
     createSelection() {
+      this.checkForPenalty();
+      this.tries.number = false;
+      this.tries.position = false;
+
       this.selectedColumn = this.getRandomPosition();
       this.selectedRow = this.getRandomPosition();
       this.selectedNumber = this.getRandomNumber();
+
+      this.pushToHistory();
+    },
+    checkForPenalty() {
+      if (this.history.length < 1 + this.nBackLevel) {
+        return 0;
+      }
+      if (this.tries.number === false && this.checkNumber()) {
+        this.points -= 1;
+      }
+      if (this.tries.position === false && this.checkPosition()) {
+        this.points -= 1;
+      }
+      return 0;
     },
     getRandomPosition() {
       return Math.floor(Math.random() * 3 + 1);
     },
     getRandomNumber() {
       return Math.floor(Math.random() * 10);
+    },
+    onKey(event) {
+      const NUMBER = "ArrowRight";
+      const POSITION = "ArrowLeft";
+      const key = event.key;
+
+      if (key === NUMBER) {
+        this.userSelectedNumber();
+      } else if (key === POSITION) {
+        this.userSelectedPosition();
+      }
+    },
+    userSelectedNumber() {
+      if (this.tries.number) {
+        return;
+      }
+      this.tries.number = true;
+      this.points += this.checkNumber() ? 1 : -1;
+      console.log(this.points);
+    },
+    userSelectedPosition() {
+      if (this.tries.position) {
+        return;
+      }
+      this.tries.position = true;
+      this.points += this.checkPosition() ? 1 : -1;
+      console.log(this.points);
+    },
+    checkNumber() {
+      const length = this.history.length;
+      return (
+        this.selectedNumber ===
+        this.history[length - 1 - this.nBackLevel].number
+      );
+    },
+    checkPosition() {
+      const length = this.history.length;
+      const target = this.history[length - 1 - this.nBackLevel];
+      return (
+        this.selectedColumn === target.column && this.selectedRow === target.row
+      );
+    },
+    pushToHistory() {
+      this.history.push({
+        column: this.selectedColumn,
+        row: this.selectedRow,
+        number: this.selectedNumber
+      });
+
+      const length = this.history.length;
+
+      if (length > this.nBackLevel + 1) {
+        this.history = this.history.splice(length - this.nBackLevel - 1);
+        console.log("nBackLevel:", this.nBackLevel, typeof this.nBackLevel);
+        console.log("history length:", this.history.length);
+      }
     }
   }
 };
@@ -81,8 +173,15 @@ export default {
   grid-column-gap: 24px;
   &__left,
   &__right {
-    height: 32px;
+    cursor: pointer;
     background-color: #cfcfcf;
+    font-weight: bolder;
+    font-size: 2em;
+    padding: 8px;
   }
+}
+.score {
+  font-size: 2em;
+  font-weight: bolder;
 }
 </style>
