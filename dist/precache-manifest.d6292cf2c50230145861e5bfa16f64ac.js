89d6a5c669fd022192741a3f9b2a70560b18be47
Peter Ajtai <peter.ajtai@soliddigital.com>
2018-12-30 01:30:22 -0800

    initial build

---
+++ dist/precache-manifest.d6292cf2c50230145861e5bfa16f64ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "a769a35ff04a44979ec9",
+    "url": "/js/chunk-vendors.31a35644.js"
+  },
+  {
+    "revision": "5d326607c8beacd5cb1b",
+    "url": "/js/app.f5ab646c.js"
+  },
+  {
+    "revision": "f63564cf6f33d5f7dcb6",
+    "url": "/js/about.7fb6bf3e.js"
+  },
+  {
+    "revision": "9812de87cddc0d8a534b304c20d1be21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d326607c8beacd5cb1b",
+    "url": "/css/app.33fd1d86.css"
+  }
+];
